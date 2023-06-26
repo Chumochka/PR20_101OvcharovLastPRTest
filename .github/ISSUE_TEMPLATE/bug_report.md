b3e0285a9a12d31c8f202bb7118c80e00c17483d
Chumochka <danpro1010@bk.ru>
2023-06-26 23:46:27 +0700

    1

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,32 @@
+---
+name: Bug report
+about: Create a report to help us improve
+
+---
+
+**Describe the bug**
+A clear and concise description of what the bug is.
+
+**To Reproduce**
+Steps to reproduce the behavior:
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**
+A clear and concise description of what you expected to happen.
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
+
+**Environment:**
+ - OS Version: [e.g. Windows 1903]
+ - Auto Dark Mode version number [e.g. 2.3]
+ - Do you use Windows .theme switcher?
+ - Has user admin rights?
+ - which antivirus software you use?
+ - what language is your Windows set to?
+
+**Additional context**
+Add any other context about the problem here.
---
+++ UnitTestProject/UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Days_since_2000_Jan_0_Test()
+        {
+            var expected = 8578;
+            var actual = AutoThemeChanger.SunDate.Days_since_2000_Jan_0(2023, 06, 26);
+            Assert.AreEqual(expected, actual);
+        }
+        //следующие функции возвращают корректные значения тригонометрических функций
+        //Почему то стандартные функцим рассчитывают некорректно
+        [TestMethod]
+        public void Sind_Test()
+        {
+            var expected = 0.5;
+            var actual = Math.Round(AutoThemeChanger.SunDate.Sind(30),4);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Cosd_Test()
+        {
+            var expected = 0.5;
+            var actual = Math.Round(AutoThemeChanger.SunDate.Cosd(60), 4);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Tand_Test()
+        {
+            var expected = 1;
+            var actual = Math.Round(AutoThemeChanger.SunDate.Tand(45), 4);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Acosd_Test()
+        {
+            var expected = 60;
+            var actual = Math.Round(AutoThemeChanger.SunDate.Acosd(0.5), 4);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
